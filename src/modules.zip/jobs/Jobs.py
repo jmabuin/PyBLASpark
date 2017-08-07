818dcdd32bf05ae56b276dda8329f014d8c2640c
jmabuin <chemaster@gmail.com>
2017-08-07 12:39:07 +0200

    Headers added

--- src/modules.zip/jobs/Jobs.py
+++ src/modules.zip/jobs/Jobs.py
@@ -1,9 +1,36 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+"""
+# Copyright 2017 José Manuel Abuín Mosquera <josemanuel.abuin@usc.es>
+#
+# This file is part of PyBLASpark.
+#
+# PyBLASpark is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# PyBLASpark is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with PyBLASpark. If not, see <http://www.gnu.org/licenses/>.
+"""
+
+# __author__ = "José M. Abuín"
+# __credits__ = ["José M. Abuín"]
+# __license__ = "GPLv3"
+# __version__ = "0.0.1"
+# __maintainer__ = "José M. Abuín"
+# __email__ = "josemanuel.abuin@usc.es"
+# __status__ = "Development"
+
 import os
 import sys
 import inspect
-#currentdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
-#parentdir = os.path.dirname(currentdir)
-#sys.path.insert(0, parentdir)
 
 sys.path.insert(0, '..')
 
--- src/modules.zip/operations/L2.py
+++ src/modules.zip/operations/L2.py
@@ -1,3 +1,33 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+"""
+# Copyright 2017 José Manuel Abuín Mosquera <josemanuel.abuin@usc.es>
+#
+# This file is part of PyBLASpark.
+#
+# PyBLASpark is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# PyBLASpark is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with PyBLASpark. If not, see <http://www.gnu.org/licenses/>.
+"""
+
+# __author__ = "José M. Abuín"
+# __credits__ = ["José M. Abuín"]
+# __license__ = "GPLv3"
+# __version__ = "0.0.1"
+# __maintainer__ = "José M. Abuín"
+# __email__ = "josemanuel.abuin@usc.es"
+# __status__ = "Development"
+
 from pyspark import SparkContext
 from pyspark.mllib.linalg import Vectors
 from pyspark.mllib.linalg import DenseVector
